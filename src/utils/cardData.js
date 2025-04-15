b12b09667598ab9505709d4809de6b7c2a6d15c5
matheushnascimento <matheus.mhddn@gmail.com>
2025-04-15 11:51:47 -0300

    feat: add new space

--- src/utils/cardData.js
+++ src/utils/cardData.js
@@ -1,4 +1,5 @@
 //#region images
+//#region belly images
 import bellyLogo from "@/assets/images/costumers/belly/logo-belly.jpg";
 import belly from "@/assets/images/costumers/belly/IMG_4205.jpg";
 import belly1 from "@/assets/images/costumers/belly/IMG_4212.jpg";
@@ -25,11 +26,11 @@ import belly21 from "@/assets/images/costumers/belly/IMG_4423.jpg";
 import belly22 from "@/assets/images/costumers/belly/IMG_4436.jpg";
 import belly23 from "@/assets/images/costumers/belly/IMG_4450.jpg";
 import belly24 from "@/assets/images/costumers/belly/IMG_4459.jpg";
-// import voOrlandoLogo from "@/assets/images/costumers/vo-orlando/logo-vo-orlando.jpg";
-// import voOrlando1 from "@/assets/images/costumers/vo-orlando/vo-orlando(1).jpg";
-// import voOrlando2 from "@/assets/images/costumers/vo-orlando/vo-orlando(2).jpg";
-// import voOrlando3 from "@/assets/images/costumers/vo-orlando/vo-orlando(3).jpg";
 //#endregion
+import voOrlandoLogo from "@/assets/images/costumers/vo-orlando/logo-vo-orlando.jpg";
+import voOrlando1 from "@/assets/images/costumers/vo-orlando/vo-orlando(1).jpg";
+import voOrlando2 from "@/assets/images/costumers/vo-orlando/vo-orlando(2).jpg";
+import voOrlando3 from "@/assets/images/costumers/vo-orlando/vo-orlando(3).jpg";
 //#endregion
 
 export default [
@@ -94,5 +95,54 @@ export default [
       '<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FSao_Paulo&showPrint=0&showTitle=0&showCalendars=0&hl=pt_BR&src=YmU4ZWNkNGU0YTI0YmFkN2E3ZTUxMTIxNTcwNmIxMjcxZTY3NDQ3ZTRkZWJiMjA2NjgwNTkyZjNhYzAzYTM2NkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F4511E" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>',
     locality_tag:
       '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3689.3199649711264!2d-47.55429582470524!3d-22.379292179629786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c7db95fbe869c1%3A0xe0638dff1ad427a4!2sChacara%20Belly!5e0!3m2!1spt-BR!2sbr!4v1744376502038!5m2!1spt-BR!2sbr"style="width:100%; height:100;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+  },
+  {
+    title: "Chácara Vô Orlando",
+    info: {
+      size: "900 m²",
+      hosting: true,
+      event_capacity: "7 pessoas",
+      description: `
+      A área de lazer conta balanço infantil, a maior piscina e uma ampla área verde para momentos de descanso e diversão em família.
+
+      Propriedade privada: 900 m²;
+      Piscina: 77 m³;
+      Área verde: gramado, pomar e jardim.
+
+      A Chácara Vô Orlando oferece churrasqueira, área coberta e acomodações para até 7 pessoas, com 2 quartos, cozinha equipada, sala com TV à cabo, 2 banheiros e 1 lavabo.
+
+      Casa: 86 m²;
+      Área de churrasco: 37 m²;
+      Alpendre: 24 m²;
+      Rede elétrica: bifásica.
+
+      Além das acomodações, a chácara oferece Wi-Fi gratuito de 200 Mbps e estacionamento interno para maior comodidade de seus hóspedes.
+      A Chácara Vô Orlando é uma opção única de hospedagem em Rio Claro, oferecendo conforto, comodidade e lazer em um só lugar.
+      `,
+
+      locality: [
+        " R. 7 JA, 847 - Jardim America, Rio Claro - SP, 13506-031",
+        "https://g.co/kgs/nbXDFXk",
+      ],
+      pool: {
+        havePool: true,
+        size: "77m³",
+      },
+      wifi: true,
+      pet: true,
+      playground: true,
+      furniture: true,
+      barbecue_area: true,
+      cooking_area: true,
+      whatsApp: true,
+      instagram: true,
+    },
+    category: "Chácara",
+    logo: voOrlandoLogo,
+    images: [voOrlando1, voOrlando2, voOrlando3],
+    scheduler_tag:
+      '<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FSao_Paulo&showPrint=0&showNav=0&showTitle=0&showDate=0&showTabs=0&showCalendars=0&showTz=0&src=c2NvcGluaG9AZ21haWwuY29t&color=%23E67C73" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>',
+    locality_tag:
+      '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3688.990529015781!2d-47.57675152470479!3d-22.391714479620973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c7db0e81d6c945%3A0xf347743e1cfaf48e!2zQ0jDgUNBUkEgVsOUIE9STEFORE8!5e0!3m2!1spt-BR!2sbr!4v1744409022684!5m2!1spt-BR!2sbr"style="width:100%; height:100;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
   },
 ];
