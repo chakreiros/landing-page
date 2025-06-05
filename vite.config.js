9740b67a5945f7a940eb2852dac594ef4b4d1c21
matheushnascimento <matheus.mhddn@gmail.com>
2025-06-05 10:00:03 -0300

    build: add `adsense plugin` to `vite.config.js` file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import adsense from "vite-plugin-adsense";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), adsense()],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
